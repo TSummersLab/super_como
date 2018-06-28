81c5614e9a7c71d2e39d4e697ba993a8d7c5fb05
The-SS <snsafaoui@gmail.com>
2018-06-27 19:29:13 -0500

    minor changes to software_installer.sh

--- setup/software_installer.sh
+++ setup/software_installer.sh
@@ -20,6 +20,17 @@ sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc)
 sudo apt-get install vlc -y
 
 # Installing ROS-Kinetic
-./helper_scripts/install_ros_kinetic.sh # install ROS Kinetic
+cd ~/super_como/setup
+rosversion ros || ./helper_scripts/install_ros_kinetic.sh # install ROS Kinetic if not installed
 ./helper_scripts/install_ros_kinetic_packages.sh # install packages for ROS Kinetic
 
+# Installing CUDA
+cuda_installation=1
+nvcc --version || cuda_installation=0
+if [ $cuda_installation = 0 ]
+then
+#    cd ~/Downloads
+    echo "Installing CUDA"
+    
+ 
+fi
