2e0b516e81ebc3185eda71e7c3a493789a164aa3
The-SS <snsafaoui@gmail.com>
2018-07-11 20:46:10 -0500

    adding catkin_tools

--- setup/software_installer.sh
+++ setup/software_installer.sh
@@ -5,6 +5,8 @@
 # Pluma
 # VLC
 # ROS Kinetic
+# ROS Kinetic packages
+# Catkin_tools
 # OpenCV 3.4
 # Python 3
 
@@ -24,6 +26,9 @@ cd ~/super_como/setup
 rosversion ros || ./helper_scripts/install_ros_kinetic.sh # install ROS Kinetic if not installed
 ./helper_scripts/install_ros_kinetic_packages.sh # install packages for ROS Kinetic
 
+# Installing catkin_tools
+sudo apt-get install python-catkin-tools -y
+
 # Installing CUDA
 cuda_installation=1
 nvcc --version || cuda_installation=0
@@ -31,6 +36,13 @@ if [ $cuda_installation = 0 ]
 then
 #    cd ~/Downloads
     echo "Installing CUDA"
-    
- 
 fi
+
+# Insalling ZED SDK for TX2
+cd ~/super_como/setup/downloads
+#wget https://download.stereolabs.com/zedsdk/2.4/tegrax2
+wget https://www.stereolabs.com/developers/downloads/archives/ZED_SDK_Linux_JTX2_v2.3.3_JP3.2.run
+chmod +x ZED_SDK_Linux_JTX2_v2.3.3_JP3.2.run
+./ZED_SDK_Linux_JTX2_v2.3.3_JP3.2.run
+
+
