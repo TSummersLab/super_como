4dd49768be90da092e1c8d71d963d0354e6416a6
The-SS <snsafaoui@gmail.com>
2018-10-31 00:04:02 +0000

    minor edit

--- workspace/devel/share/zed_wrapper/cmake/zed_wrapper-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(zed_wrapper_MESSAGE_FILES "")
-set(zed_wrapper_SERVICE_FILES "/home/nvidia/super_como/workspace/src/zed-ros-wrapper/zed_wrapper/srv/set_initial_pose.srv;/home/nvidia/super_como/workspace/src/zed-ros-wrapper/zed_wrapper/srv/reset_odometry.srv;/home/nvidia/super_como/workspace/src/zed-ros-wrapper/zed_wrapper/srv/reset_tracking.srv")
